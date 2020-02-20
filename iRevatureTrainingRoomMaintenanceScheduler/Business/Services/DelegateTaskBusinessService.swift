37c0a7e1f322f39339dfa4815459562578491181
Kyle Keck <keckkyle@gmail.com>
2020-02-20 16:29:03 -0500

    Add DelegateTaskProtocol,DelegateTaskBusinessService

---
+++ iRevatureTrainingRoomMaintenanceScheduler/Business/Services/DelegateTaskBusinessService.swift
@@ -0,0 +1,17 @@
+//
+//  DelegateTaskBusinessService.swift
+//  iRevatureTrainingRoomMaintenanceScheduler
+//
+//  Created by Kyle Keck on 2/20/20.
+//  Copyright © 2020 revature. All rights reserved.
+//
+
+import Foundation
+
+class DelegateClassTaskProtocol: DelegateTaskProtocol {
+    
+    static func createNewTaskDelegation(room: String, endDate: Date, trainer: String) {
+        //code create new TaskDelegation
+    }
+    
+}

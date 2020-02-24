d492266e231b36a472a184fe5dcd4369ee6d9930
Kyle Keck <keckkyle@gmail.com>
2020-02-24 16:48:20 -0500

    Update task functions in service layer

--- iRevatureTrainingRoomMaintenanceScheduler/Application/Modules/View-Controllers/ViewController.swift
+++ iRevatureTrainingRoomMaintenanceScheduler/Application/Modules/View-Controllers/ViewController.swift
@@ -30,7 +30,8 @@ class ViewController: UIViewController {
             passwordTextField.delegate = self
         }
         
-        CreateAllTables.runScript()
+
+//        CreateAllTables.runScript()
         
 //        let filePath = DatabaseAccess.getDatabaseFilePath(name: DatabaseInfo.databaseName, pathDirectory: DatabaseInfo.databaseDirectory, domainMask: DatabaseInfo.databaseDomainMask)
 //
@@ -45,8 +46,8 @@ class ViewController: UIViewController {
         self.login()
     }
     
+    
     func login(){
-        
         let userEmail = emailTextField.text!
         let userPassword = passwordTextField.text!
         validateUser(email: userEmail, password: userPassword)
@@ -61,6 +62,7 @@ class ViewController: UIViewController {
         if(email == "testuser1@revature.com" && password == "test123"){
             
             let loginapi = LoginAPI()
+            
             loginapi.getUserLogin(email: email, password: password, completionHandler:  { user in
                 let userData = User(id: 0, email: email, name: email, role: user.currentSystemRole.name, token: user.loginToken, keepLoggedIn: keepLoggedIn)
                 if UserInfoBusinessService.setUserInfo(userObject: userData) {

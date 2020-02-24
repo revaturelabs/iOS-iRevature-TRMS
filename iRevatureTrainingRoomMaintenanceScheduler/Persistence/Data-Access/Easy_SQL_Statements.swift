78fe3ad2a6d30c87e1bb885d6e3059a06e780ab6
Mark Hawkins <markhawkins@USF-Gold-Wifi-226-101-27.laptops.usf.edu>
2020-02-24 11:48:27 -0500

    Added get report data from sqlite, added get room task data from sqlite

---
+++ iRevatureTrainingRoomMaintenanceScheduler/Persistence/Data-Access/Easy_SQL_Statements.swift
@@ -0,0 +1,54 @@
+//
+//  Easy_SQL_Statements.swift
+//  iRevatureTrainingRoomMaintenanceScheduler
+//
+//  Created by Mark Hawkins on 2/24/20.
+//  Copyright © 2020 revature. All rights reserved.
+//
+
+import Foundation
+
+extension MaintenanceChartTable {
+    
+    static func getMaintenanceChartRangeStatement(roomID: Int, startDate: Date, endDate: Date) -> SelectStatement {
+        let dateFormat = SQLiteDateFormat.dateFormatter
+        
+        var selectStatement = SelectStatement()
+        selectStatement.specifyColumn(table: table, columnName: ColumnName.id.rawValue, asName: "id")
+        selectStatement.specifyColumn(table: table, columnName: ColumnName.date.rawValue, asName: "date")
+        selectStatement.specifyColumn(table: table, columnName: ColumnName.cleaned.rawValue, asName: "cleaned")
+        
+        var whereStatement = WhereStatement()
+        whereStatement.addStatement(table: table, columnName: ColumnName.date.rawValue, expression: .BETWEEN, columnValue: dateFormat.string(from: startDate), dateFormat.string(from: endDate))
+        whereStatement.addStatement(table: table, clause: .AND, columnName: ColumnName.roomID.rawValue, expression: .EQUALS, columnValue: roomID)
+        
+        
+        selectStatement.setWhereStatement(statement: whereStatement)
+        
+        return selectStatement
+    }
+}
+
+extension RoomTaskTable {
+    static func getTasksByDate(roomID: Int, date: Date) -> SelectStatement {
+        let dateFormat = SQLiteDateFormat.dateFormatter
+        
+        var selectStatement = SelectStatement()
+        selectStatement.specifyColumn(table: table, columnName: ColumnName.id.rawValue, asName: "room_task_id")
+        selectStatement.specifyColumn(table: TaskTable.table, columnName: TaskTable.ColumnName.id.rawValue, asName: "task_id")
+        selectStatement.specifyColumn(table: TaskTable.table, columnName: TaskTable.ColumnName.name.rawValue, asName: "task_name")
+        
+        let joinStatement = JoinStatement(table1: table, joinType: .INNER, table2: TaskTable.table, onColumnName1: ColumnName.taskID.rawValue, onColumnName2: TaskTable.ColumnName.id.rawValue)
+        
+        var whereStatement = WhereStatement()
+        whereStatement.addStatement(table: table, columnName: ColumnName.dateStart.rawValue, expression: .LESSTHAN, columnValue: dateFormat.string(from: date))
+        whereStatement.addStatement(table: table, clause: .AND, columnName: ColumnName.dateEnd.rawValue, expression: .GREATERTHAN, columnValue: dateFormat.string(from: date))
+        
+        selectStatement.setJoinStatement(statement: joinStatement)
+        selectStatement.setWhereStatement(statement: whereStatement)
+        
+        print(selectStatement.makeStatement()!)
+        
+        return selectStatement
+    }
+}

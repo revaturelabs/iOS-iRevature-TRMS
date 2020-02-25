89152f32c1059c5129db443457dbae3f3578cd8b
Kyle Keck <keckkyle@gmail.com>
2020-02-25 15:18:24 -0500

    updates

--- iRevatureTrainingRoomMaintenanceScheduler/Application/Modules/View-Controllers/DelegateTaskViewController.swift
+++ iRevatureTrainingRoomMaintenanceScheduler/Application/Modules/View-Controllers/DelegateTaskViewController.swift
@@ -12,25 +12,21 @@ class DelegateTaskViewController: UIViewController {
     
     @IBOutlet weak var dateSelection: UITextField!
     @IBOutlet weak var roomSelection: UITextField!
-    @IBOutlet weak var trainerSelection: UITextField!
+    @IBOutlet weak var reasonTextView: UITextView!
     
     
     
     var roomList = [RoomName]()
     
-    var trainerList = [Trainer]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         roomList = RoomBusinessService.getAllRooms()
-        trainerList = TrainerBusinessService.getAllTrainers()
         
         dateSelection.dateDropDown(dateFormat: "MMM dd, yy")
         roomSelection.showDropDown(data: roomList.map{$0.name})
-        trainerSelection.showDropDown(data: trainerList.count == 0 ? ["No trainers available"] : trainerList.map{$0.name})
-        
-
+    
     }
     
     @IBAction func assignTask(_ sender: Any) {
@@ -38,6 +34,5 @@ class DelegateTaskViewController: UIViewController {
         formatter.dateFormat = "MMM dd, yy"
         let date = formatter.date(from: dateSelection.text!)!
         
-        DelegateTaskBusinessService.createNewTaskDelegation(room: roomSelection.text!, date: date, trainer: trainerSelection.text!)
     }
 }
